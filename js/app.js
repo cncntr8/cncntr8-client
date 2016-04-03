19eb272e1a7997cc44e84523b3ffa6c9568e5cae
Jonathan Lee <Jonwlee457@gmail.com>
2016-04-02 23:15:39 -0400

    spaghetti

--- js/app.js
+++ js/app.js
@@ -1,42 +1,65 @@
-var exampleSocket = new WebSocket("ws://localhost:8000");
+//var exampleSocket = new WebSocket("ws://localhost:8000");
+var spanNumber = document.getElementsByTagName("span");
+var userNumber = document.getElementsByTagName("h5");
+var rando = 0;
+var color = "";
+var metric = "concentration";
+var metricIndex = 0;
+var name = [];
+updateProgress();
+updateUsers();
+
 /*
 exampleSocket.onmessage = function(event) {
-    var f = document.getElementById("chatbox").contentDocument;
     var text = "";
     var msg = JSON.parse(event.data);
 
     switch (msg.type) {
-        case "id":
-            clientID = msg.id;
-            setUsername();
+        case "users":
+            name = msg.fullname;
+            break;
+        case "concentration":
+            metric = msg.type;
+            metricIndex = 0;
+            break;
+        case "mellowness":
+            metric = msg.type;
+            metricIndex = 1;
             break;
     }
-    document.getElementById("userlistbox").innerHTML = ul;
-}
-
-if (text.length) {
-    f.write(text);
-    document.getElementById("chatbox").contentWindow.scrollByPages(1);
 };
 */
-exampleSocket.close();
-
-for (var i=0; i<4;i++) {
-	var rando =parseInt(Math.random() * 100);
-	var color = "";
-	if (rando<=30){
-		color = "progress alert"	
-	}
-	if (rando>30 && rando<=70){
-		color = "progress warning"	
-	}
-	if (rando>70 && rando<=100){
-		color = "progress success"	
-	}
-
-	//25% placeholder for integer from 
-	document.getElementsByTagName("span")[i].setAttribute("style", ("width:" +rando +"%")); 
-	document.getElementsByClassName("progress-meter-text")[i].innerHTML=(rando+"%"); 
-	document.getElementsByClassName("progress")[i].className =color;
-
+function generateRandom(){
+	rando = parseInt(Math.random() * 100);
 }
+
+function updateProgress(){
+	for (var i=0; i<spanNumber;i++) {
+		generateRandom();
+
+		if (rando<=30){
+			color = "progress alert"	
+		}
+		if (rando>30 && rando<=70){
+			color = "progress warning"	
+		}
+		if (rando>70 && rando<=100){
+			color = "progress success"	
+		}
+
+		//25% placeholder for integer from 
+		document.getElementsByTagName("span")[i].setAttribute("style", ("width:" +rando +"%"));
+		document.getElementsByClassName("text")[i].innerHTML=metric; 
+		document.getElementsByClassName("progress-meter-text").querySelectorAll("p")[(metricIndex)].innerHTML=(rando+"%"); 
+		document.getElementsByClassName("progress")[i].querySelectorAll("span")[metricIndex].className = color;
+
+	}
+}
+
+function updateUsers(){
+	for (var i=0; i<userNumber; i++)
+		document.getElementsByTagName("h5")[i].value = name[i];
+}
+
+//exampleSocket.close();
+
